c9270f52b017b898d7558698f47ab0ea8581488e
unknown <nazmulnobel000@gmail.com>
2022-11-27 18:11:17 +0600

    catagory update

--- src/Home/Catagories/Catagory.js
+++ src/Home/Catagories/Catagory.js
@@ -9,7 +9,7 @@ const Catagory = ({ catagory }) => {
             <div className="card card-compact w-96 bg-base-100 shadow-xl">
                 <figure><img src={image} alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">{title}</h2>
+                    <h2 className="card-title">Brand Name: {title}</h2>
                     <div className="card-actions justify-end">
                         <Link to={`/catagory/${catagory_id}`}><button className="btn btn-primary">View More</button></Link>
                     </div>
